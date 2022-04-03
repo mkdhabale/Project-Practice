62669d7a519b4fa9b8a223b28c4accca2de1e8ee
Mukul Dhabale <mukul.dhabale@gmail.com>
2022-04-03 19:14:40 +0530

    Private Typescript Project Removed
    Private Typescript Project Removed

--- TypeScript/Practice1/app.js
+++
@@ -1,6 +0,0 @@
-var a = "Hello TS12352";
-console.warn(a);
-function users(data) {
-    return data;
-}
-console.warn(users(5));
--- TypeScript/Practice1/app.ts
+++
@@ -1,8 +0,0 @@
-let a: number = "Hello TS12352";
-console.warn(a);
-
-function users<T>(data: T): T {
-    return data;
-}
-
-console.warn(users(5));
